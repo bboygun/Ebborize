8bd3835c6faae036d22fe83e41eea696726bf8c2
heguanqiao <hegq1123@gmail.com>
2021-08-24 23:12:46 +0800

    创建项目

---
+++ main/java/org/airflare/ebborize/EbborizeApplication.java
@@ -0,0 +1,13 @@
+package org.airflare.ebborize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbborizeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EbborizeApplication.class, args);
+    }
+
+}
